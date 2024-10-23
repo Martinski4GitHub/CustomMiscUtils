a8bdffe9e9c3fe8379166a55257692ff782cd5b2
Martinski <119833648+Martinski4GitHub@users.noreply.github.com>
2024-10-23 01:49:55 -0700

    Update SQLite3_Trim_TEST.sh
    More code to gather diagnostics while running the trim tests.

--- Diags/SQLite3_Trim_TEST.sh
+++ Diags/SQLite3_Trim_TEST.sh
@@ -2,10 +2,10 @@
 ###################################################################
 # SQLite3_Trim_TEST.sh
 # Creation Date: 2024-Oct-20 [Martinski W.]
-# Last Modified: 2024-Oct-22 [Martinski W.]
+# Last Modified: 2024-Oct-23 [Martinski W.]
 ###################################################################
 
-VERSION="0.1.3"
+VERSION="0.1.4"
 
 if true
 then readonly LOG_DIR_PATH="$HOME"
@@ -17,6 +17,10 @@ readonly sqLiteDBaseFile="${OPT_USB_PATH}/dnsqueries.db"
 readonly sqLiteDBcmdFile="${LOG_DIR_PATH}/uiDivStats_SQLDB_Trim.SQL"
 readonly sqLiteDBlogFile="${LOG_DIR_PATH}/uiDivStats_SQLDB_Trim.LOG"
 readonly logTimeFormat="%Y-%m-%d %H:%M:%S"
+readonly _1_0_GB_="1073741824"
+readonly _1_5_GB_="1610612736"
+readonly _2_0_GB_="2147483648"
+readonly maxTrimSize="$_2_0_GB_"
 
 _DaysToKeep_() { echo "$daysToKeepData" ; }
 
@@ -36,17 +40,46 @@ _SetSQLDBcmdsFile_()
    } > "$sqLiteDBcmdFile"
 }
 
+_ShowSQLDBcmdsFile_()
+{
+  cat "$sqLiteDBcmdFile"
+  echo "=========================================================="
+}
+
 _ShowSQLDBfileInfo_()
 {
+   [ ! -s "$1" ] && return 1
    local fileSize  fileInfo
    fileSize="$(ls -1lh "$1" | awk -F ' ' '{print $3}')"
    fileInfo="$(ls -1l "$1" | awk -F ' ' '{print $3,$4,$5,$6,$7}')"
-   printf "[%s] %s\n" "$fileSize" "$fileInfo"
+   printf "[%sB] %s\n" "$fileSize" "$fileInfo"
+}
+
+_ShowMemInfo_()
+{
+   local infoType
+   if [ $# -eq 0 ] || [ -z "$1" ]
+   then infoType=1
+   else infoType="$1"
+   fi
+   if [ "$infoType" -eq 1 ]
+   then printf "free:\n" ; free
+   elif [ "$infoType" -eq 2 ]
+   then
+      printf "---------\n MemInfo\n---------\n"
+      grep -E '^Mem[TFA].*:[[:blank:]]+.*' /proc/meminfo
+      grep -E '^(Buffers|Cached):[[:blank:]]+.*' /proc/meminfo
+      grep -E '^Swap[TFC].*:[[:blank:]]+.*' /proc/meminfo
+      grep -E '^(Active|Inactive)(\([af].*\))?:[[:blank:]]+.*' /proc/meminfo
+      grep -E '^(Dirty|Writeback|AnonPages|Unevictable):[[:blank:]]+.*' /proc/meminfo
+   fi
+   echo "----------------------------------------"
 }
 
 _TrimDatabase_()
 {
-   local timeNowSecs  numCount  maxCount  errorCheck
+   local timeNowSecs  dbFileSize  triesCount  maxTriesCount  resultStr
+   local errorCheck  errorCount  maxErrorCount  foundError  foundLocked
 
    if [ -s "$sqLiteDBlogFile" ]
    then
@@ -59,55 +92,86 @@ _TrimDatabase_()
    export TZ
 
    printf "[$(date +"$logTimeFormat")] BEGIN.\n" >> "$sqLiteDBlogFile"
-   [ -s "$sqLiteDBaseFile" ] && _ShowSQLDBfileInfo_ "$sqLiteDBaseFile" | tee -a "$sqLiteDBlogFile"
+   _ShowSQLDBfileInfo_ "$sqLiteDBaseFile" | tee -a "$sqLiteDBlogFile"
    printf "[$(date +"$logTimeFormat")] Trimming records older than [$daysToKeepData] days from database.\n" | tee -a "$sqLiteDBlogFile"
    echo "----------------------------------------------------------" >> "$sqLiteDBlogFile"
+   _ShowMemInfo_ 1 >> "$sqLiteDBlogFile"
 
-   numCount=0 ; maxCount=5
+   foundError=false
+   foundLocked=false
+   triesCount=0 ; maxTriesCount=10
+   errorCount=0 ; maxErrorCount=5
+   dbFileSize=0
+
+   [ -s "$sqLiteDBaseFile" ] && \
+   dbFileSize="$(ls -1l "$sqLiteDBaseFile" | awk -F ' ' '{print $3}')"
+   if [ "$dbFileSize" -ge "$maxTrimSize" ]
+   then vacuumOK=false ; cacheSize=10 ; fi
    timeNowSecs="$(date +'%s')"
    _SetSQLDBcmdsFile_
    trap '' HUP
 
-   while ! "$sqLiteDBbinPath" "$sqLiteDBaseFile" < "$sqLiteDBcmdFile" >> "$sqLiteDBlogFile" 2>&1
+   while [ "$((triesCount++))" -lt "$maxTriesCount" ] && [ "$errorCount" -lt "$maxErrorCount" ]
    do
-      numCount="$((numCount + 1))"
+      if "$sqLiteDBbinPath" "$sqLiteDBaseFile" < "$sqLiteDBcmdFile" >> "$sqLiteDBlogFile" 2>&1
+      then foundError=false ; foundLocked=false ; break ; fi
       errorCheck="$(tail -n1 "$sqLiteDBlogFile")"
-      printf "[$(date +"$logTimeFormat")] TRY_COUNT=[$numCount]\n" | tee -a "$sqLiteDBlogFile"
+      echo "-----------------------------------" >> "$sqLiteDBlogFile"
+      printf "[$(date +"$logTimeFormat")] TRY_COUNT=[$triesCount]\n" | tee -a "$sqLiteDBlogFile"
       if echo "$errorCheck" | grep -qE "^(Error:|Parse error|Runtime error)"
       then
-         echo "$errorCheck" ; foundError=true
-         {
-           echo "------------------------------------"
-           cat "$sqLiteDBcmdFile"
-           echo "=========================================================="
-         } >> "$sqLiteDBlogFile"
-         [ "$numCount" -gt 0 ] && limitSize=0
-         [ "$numCount" -gt 1 ] && cacheSize=10
-         [ "$numCount" -gt 2 ] && limitSize=1000
-         [ "$numCount" -gt 3 ] && vacuumOK=false
+         echo "$errorCheck"
+         if echo "$errorCheck" | grep -qE "^Runtime error .*: database is locked"
+         then foundLocked=true ; continue ; fi
+         errorCount="$((errorCount + 1))" ; foundError=true ; foundLocked=false
+         _ShowMemInfo_ 2 >> "$sqLiteDBlogFile"
+         _ShowSQLDBcmdsFile_ >> "$sqLiteDBlogFile"
+         if "$vacuumOK"
+         then [ "$errorCount" -eq 1 ] && vacuumOK=false
+         else [ "$errorCount" -eq 1 ] && limitSize=0
+         fi
+         if [ "$limitSize" -gt 0 ]
+         then [ "$errorCount" -eq 2 ] && limitSize=0
+         else [ "$errorCount" -eq 2 ] && cacheSize=5
+         fi
+         if [ "$cacheSize" -gt 5 ]
+         then [ "$errorCount" -eq 3 ] && cacheSize=5
+         else [ "$errorCount" -eq 3 ] && limitSize=1000
+         fi
+         if [ "$limitSize" -eq 1000 ]
+         then maxErrorCount=4
+         else [ "$errorCount" -eq 4 ] && limitSize=1000
+         fi
          _SetSQLDBcmdsFile_
       fi
-      [ "$numCount" -ge "$maxCount" ] && break
+      [ "$triesCount" -ge "$maxTriesCount" ] && break
+      [ "$errorCount" -ge "$maxErrorCount" ] && break
       sleep 1
    done
 
+   if "$foundError"
+   then resultStr="reported error(s)."
+   elif "$foundLocked"
+   then resultStr="found locked database."
+   else
+      resultStr="completed successfully."
+      printf "[$(date +"$logTimeFormat")] TRY_COUNT=[$triesCount]\n" | tee -a "$sqLiteDBlogFile"
+      _ShowSQLDBcmdsFile_ >> "$sqLiteDBlogFile"
+   fi
    ! "$doDebug" && rm -f "$sqLiteDBcmdFile"
-   "$foundError" && resultStr="reported error(s)." || resultStr="completed successfully."
+
+   _ShowMemInfo_ 1 >> "$sqLiteDBlogFile"
    printf "[$(date +"$logTimeFormat")] Database trim process ${resultStr}\n" | tee -a "$sqLiteDBlogFile"
-   [ -s "$sqLiteDBaseFile" ] && _ShowSQLDBfileInfo_ "$sqLiteDBaseFile" | tee -a "$sqLiteDBlogFile"
+   _ShowSQLDBfileInfo_ "$sqLiteDBaseFile" | tee -a "$sqLiteDBlogFile"
    printf "[$(date +"$logTimeFormat")] END.\n" >> "$sqLiteDBlogFile"
 }
 
-onceOK=true
 doDebug=false
 vacuumOK=true
 cacheSize=20
 limitSize=2000
-foundError=false
 doSrceFrom=true
 daysToKeepData=30
-
-trap 'exit 0' INT QUIT ABRT TERM
 
 if [ $# -eq 0 ] || [ -z "$1" ]
 then
@@ -118,9 +182,8 @@ else
    do
       case "$PARAM" in
           "debug") doDebug=true ;;
-          "nolim") limitSize=0 ;;
           "cache") cacheSize=10 ;;
-          "nosrc") doSrceFrom=false ;;
+          "novac") vacuumOK=false ;;
                 *) if echo "$1" | grep -qE "^([1-9][0-9]{0,2})$"
                    then
                       daysToKeepData="$1"
@@ -133,9 +196,9 @@ else
    done
 fi
 
+trap 'exit 0' INT QUIT ABRT TERM
 _TrimDatabase_
 printf "\nLogfile [$sqLiteDBlogFile] was created.\n"
-
 exit 0
 
 #EOF#
